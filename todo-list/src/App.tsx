dbb61c215813dc627b1bfafa545fd4801745f809
TiboMessiaen <tibo.messiaen@student.howest.be>
2023-03-06 17:39:45 +0100

    fix: todos toevoegen

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,11 +1,43 @@
+import { useState } from "react";
+
+import { ITodo } from "./interfaces/ITodo";
+
 import AddTodo from "./components/AddTodo";
 import AppFooter from "./components/AppFooter";
 import AppHeader from "./components/AppHeader";
 import TodoItem from "./components/TodoItem";
 
+
+
 function App() {
+  const [todos, setTodos] = useState<ITodo[]>([
+    { id: '1', name: "Learn React", category: "work", isDone: true },
+    { id: '2', name: "Learn TypeScript", category: "work", isDone: false },
+    { id: '3', name: "Learn GraphQL", category: "work", isDone: false },
+  ]);
+
+  const addNewTodo = (t: ITodo) => {
+    setTodos([t, ...todos]);
+  }
+
   return (
-    
+    <div className="o-container">
+      <AppHeader />
+
+      <AddTodo 
+        handleNewTodo={addNewTodo} 
+      />
+
+      <ul>
+        {todos.map((todo: ITodo) => (
+          <TodoItem
+          todo={todo} key={todo.name}
+        />
+        ))}
+      </ul>
+
+      <AppFooter />
+    </div>
   );
 }
 
